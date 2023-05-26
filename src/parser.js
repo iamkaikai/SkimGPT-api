b6afddc0bbfbb49498b0543a0beb3ec547ee0f30
Annie Tang <annie.tang.25@dartmouth.edu>
2023-05-26 11:59:53 -0400

    added id refs to html and summary sections

--- src/parser.js
+++ src/parser.js
@@ -46,23 +46,23 @@ async function fetchAndParseURL(URL) {
                 line_counter++;
             });
             
-            // this isnt working yet:
-            // update the parser.html header to include an anchor id for each section
-            // loop through sections_anchors and find the corresponding element in result_html_lines
-            // replace the element with the element + anchor id
-            // section_anchors.forEach(element => {
-            //     let regex = /(<h1|<h2|<h3|<h4)([^>]*)>(.*?)<\/h[1-4]>/;
-            //     let match = element.match(regex);
-            //     if (match) {
-            //         keyword = match[3];
-            //         regex_txt = new RegExp('\\b' + keyword + '\\b', 'g');
-            //         anchor = '<a href="#section' + i + '">' + keyword + '</a>';
-            //         result_html_lines[target_line] = result_html_lines[target_line].replace(regex_txt, '\n----\n\n\n' + '<section id=section' + i + '>' + anchor + '</section>\n');
-            //         i++;
-            //     }
-            // });
+            new_result_html = '';
+            let j = 1;
 
-            // result_html = result_title + "\n" + result_html_lines.join("\n");
+            result_html_lines.forEach(element => {
+                if (section_anchors.includes(element)) {
+                    let regex = /(<h1|<h2|<h3|<h4)([^>]*)>(.*?)<\/(h[1-4])>/;
+                    let match = element.match(regex);
+                    let keyword = match[3];
+                    let anchor = '<a href="#section' + j + '">' + keyword + '</a>';
+                    new_result_html += element.replace(regex, '$1$2 section id=reader-view' + j + '>' + anchor + '</$4>') + '\n';
+                    j++;
+                } else {
+                    new_result_html += element + '\n';
+                }
+            });
+
+            result_html = new_result_html;
 
             result_text = result_text_lines.join("\n");
             save(result_text, './output/parser.txt');

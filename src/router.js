2a42019e5f50806bb59e087556fdcf6541945b2b
Annie Tang <annie.tang.25@dartmouth.edu>
2023-05-29 13:58:04 -0400

    updated router paths

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 });
 
 // summarizer routes
-router.route('/summarizer')
+router.route('/summarizers')
   .post(async (req, res) => {
     const initInfo = req.body;
     try {
@@ -19,21 +19,21 @@ router.route('/summarizer')
     }
   })
   .get(async (req, res) => {
-    const initInfo = req.body;
     try {
-      const result = await Summarizer.getSummarizer(initInfo);
+      const result = await Summarizer.getSummarizers();
       return res.json(result);
     } catch (error) {
       return res.status(404).json({ error });
     }
   });
 
-// section routes
-router.route('/summarizer/:id')
+// individual summarizer routes
+router.route('/summarizers/:id')
   .get(async (req, res) => {
     try {
-      console.log('inside get section');
-      const result = await Summarizer.getSection(req.params.id);
+      console.log('inside get individual summarizer');
+      console.log(req.body);
+      const result = await Summarizer.getSection(req.body);
       return res.json(result);
     } catch (error) {
       return res.status(404).json({ error });
